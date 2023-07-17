d460a6d39c3151e458cad10e73495115c2ef2438
Amira337 <138816787+Amira337@users.noreply.github.com>
2023-07-17 18:53:57 +0300

    Add files via upload

--- shiny_lahdi1.ino
+++ shiny_lahdi1.ino
@@ -2,12 +2,14 @@
 int SS1=2; // SlidSwitch
 int SS2=3; // SlidSwitch
 int R=0;// Reading
-int C=0;// Counter
+
 int I=0;// Intensity
 int L1=5;
 int L2=6;
 int L3=7;
 int L4=8;
+int L5=9;
+int L6=10;
   void setup()
 {
   
@@ -15,6 +17,8 @@ int L4=8;
   pinMode(L2, OUTPUT);
   pinMode(L3, OUTPUT);
   pinMode(L4, OUTPUT);
+  pinMode(L5, OUTPUT);
+  pinMode(L6, OUTPUT);
   pinMode(SS1, INPUT);
   pinMode(SS2, INPUT);
     
@@ -22,45 +26,41 @@ int L4=8;
 
 void loop()
 {
-  
-   if (SS1==HIGH)
+  R=digitalRead(SS1);
+   if (R==HIGH)
   {
-    R=digitalRead;
-    if (R==HIGH)
-    {
-   C++;
+   
   digitalWrite(8, HIGH);
   for (I=0;I<=128;I++)
   {analogWrite(5,HIGH);}
-   while (SS1==HIGH);
+   
   }
-   }
+   
   else
   {
     digitalWrite(8, LOW);
-  for (I=128;I<=0;I++)
-  {analogWrite(2,LOW);}
+    digitalWrite(9, LOW);
+  for (I=128;I>=0;I++)
+  {analogWrite(5,LOW);}
    while (SS1==LOW);
-   C=0;
+   
   }
-   if (SS2==HIGH)
-  {
-     R=digitalRead;
-     if (R==HIGH)
-     {
-   C++;
+  R=digitalRead(SS2);
+   if (R==HIGH)
+   {
   digitalWrite(7, HIGH);
   for (I=0;I<=128;I++)
   {analogWrite(6,HIGH);}
-   while (SS2==HIGH);
+  
   }
-   }
+   
   else
   {
     digitalWrite(7, LOW);
-  for (I=128;I<=0;I++)
+    digitalWrite(10, LOW);
+  for (I=128;I>=0;I++)
   {analogWrite(6,LOW);}
    while (SS2==LOW);
-   C=0;
+  
   }
 }
